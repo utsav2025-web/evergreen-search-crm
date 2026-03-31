288a32e52d7e56d3f2b0e511bb13ee5dba34c0ed
utsav2025-web <ughosh@mba2027.hbs.edu>
2026-03-30 22:16:07 -0400

    Add migration: add stage_entered_at column to companies table

---
+++ backend/alembic/versions/f1e2d3c4b5a6_add_stage_entered_at_to_companies.py
@@ -0,0 +1,23 @@
+"""add stage_entered_at to companies
+
+Revision ID: f1e2d3c4b5a6
+Revises: 0f8d4fdcc269
+Create Date: 2026-03-30
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'f1e2d3c4b5a6'
+down_revision = '0f8d4fdcc269'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+      op.add_column('companies', sa.Column('stage_entered_at', sa.DateTime(timezone=True), nullable=True))
+
+
+def downgrade() -> None:
+      op.drop_column('companies', 'stage_entered_at')
